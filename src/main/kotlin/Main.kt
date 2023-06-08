baeeaabe1c0fe302ecc853d04a3f15bc38059d9e
Dmitry Titov <ditriy.titov@mail.ru>
2023-06-08 09:54:10 +0500

    Лучший 83

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 data class Post(
     var id: Int,
 
-    var likes: Likes = Likes(0),
+    var likes: Likes = Likes(2),
     var stringData: String = "",
     var booleanData: Boolean = false,
     var byteData: Byte = 0,
@@ -23,28 +23,28 @@ object WallService {
     private var posts = arrayOf<Post>()
     private var lastId = 0
     fun add(post: Post): Post {
-        posts += post.copy(id = ++lastId, likes = post.likes.copy())
-        return posts.last()
+        val newPost = post.copy(id = ++lastId, likes = post.likes.copy())
+        posts += newPost
+        return newPost
 
 
     }
 
     fun update(newPost: Post): Boolean {
-
-        for ((index, post) in posts.withIndex()) {
-            if (post.id == newPost.id) {
+        for (index in posts.indices) {
+            if (posts[index].id == newPost.id) {
                 posts[index] = newPost.copy(likes = newPost.likes.copy())
                 return true
             }
         }
         return false
-
     }
+
 
     fun getById(id: Int): Post? {
         return posts.find { it.id == id }
-
     }
+
 
     fun clear() {
         posts = emptyArray()
@@ -61,13 +61,13 @@ object WallService {
 }
 
 fun main() {
-    val post = Post(0)
+    val post = Post(1)
     WallService.add(post)
     WallService.add(Post(1))
     WallService.add(Post(2))
     WallService.print()
     post.id = 2
     WallService.print()
-    //WallService.update(Post(2, Likes(12)))
-    //WallService.print()
+    WallService.update(Post(2, Likes(12)))
+    WallService.print()
 }
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -2,13 +2,16 @@ import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 
+
 class MainKtTest {
+
 
 
     @Before
     fun clearBeforeTest() {
         WallService.clear()
     }
+
 
     @Test
     fun testAddPost() {
@@ -73,14 +76,34 @@ class MainKtTest {
     @Test
     fun main1() {
 
-        val main1 = main()
+      main()
 
     }
 
 
 
 
+    @Test
+    fun testUpdateNonExistingPost6() {
+        val updatedPost = Post(2, Likes(12)) // Создаем новый пост с обновленными данными
+        val result = WallService.update(updatedPost) // Обновляем пост в WallService
+        assertFalse(result) // Проверяем, что обновление не удалось, так как пост с таким ID не существует
+        val retrievedPost = WallService.getById(updatedPost.id) // Получаем пост из WallService по заданному ID
+        assertNull(retrievedPost) // Проверяем, что полученный пост является null
+    }
+
+
+    @Test
+    fun testClearPosts() {
+        val post = Post(1)
+        WallService.add(post)
+        WallService.clear()
+        val retrievedPost = WallService.getById(post.id)
+        assertNull(retrievedPost)
+    }
 
 
 }
+
+
 
